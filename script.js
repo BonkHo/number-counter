b0069557f9e201826ebc6eeb5d50b0b491b6bb14
John <John@DESKTOP-SRO1BKN>
2021-06-23 00:13:44 -0700

    Updated 6-23-2021

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("counter-number").innerHTML = 0;
+
+function incrementCounter() {
+    counter++;
+    document.getElementById("counter-number").innerHTML = counter;
+}
+
+function decrementCounter() {
+    counter--;
+    document.getElementById("counter-number").innerHTML = counter;
+}
+
+function resetCounter() {
+    counter = 0;
+    document.getElementById("counter-number").innerHTML = counter;
+}
